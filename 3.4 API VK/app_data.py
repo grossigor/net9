acce513753c304b47f7f9a21d161d32e027baef4
DESKTOP-NKAIR8G\igorg <igor_gross_work@outlook.com>
2017-11-15 01:13:35 +0700

    Выполнение домашней работы 3.4 «Работа с API ВК, json, протокол OAuth»

---
+++ 3.4 API VK/app_data.py
@@ -0,0 +1,26 @@
+import requests
+from time import sleep
+
+VERSION = '5.69'
+TOKEN = '0fb186691a6036fc462d03ee480447c8decf248813fa38a93cf2542319867bc6b4e2edc729bbf90dac47b'
+
+
+def get_user_friends_list(user_id=None):
+    params = {
+        'access_token': TOKEN,
+        'v': VERSION,
+        'user_id': user_id,
+    }
+    sleep(0.35)
+    response = requests.get('https://api.vk.com/method/friends.get', params)
+    response_json = response.json()
+    try:
+        return response_json['response']['items']
+    except KeyError:
+        return None
+
+
+if __name__ == '__main__':
+    my_friends_list = get_user_friends_list()
+    user_friends_list = [set(get_user_friends_list(user_id)) for user_id in my_friends_list]
+    print(set.intersection(*user_friends_list))
