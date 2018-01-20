4ba6ae4e5e39f31ab87dce74a8d765c979bc6262
DESKTOP-NKAIR8G\igorg <igor_gross_work@outlook.com>
2018-01-20 23:25:36 +0700

    Правка замечаний

--- diploma/diploma.py
+++ diploma/diploma.py
@@ -1,20 +1,43 @@
+import json
 import requests
 from time import sleep
 
 VERSION = '5.69'
-TOKEN = '5dfd6b0dee902310df772082421968f4c06443abecbc082a8440cb18910a56daca73ac8d04b25154a1128'
+TOKEN = 'secret'
+
+
+def get_token():
+    with open("token.json") as file:
+        data = json.load(file)
+        return data["TOKEN"]
+
+
+def do_request(method, **kwargs):
+    params = {
+        'access_token': TOKEN,
+        'v': VERSION
+    }
+    params.update(kwargs)
+    while True:
+        try:
+            print('-')
+            response = requests.get(method, params)
+            response.raise_for_status()
+            response_json = response.json()
+            if 'error' in response_json.keys():
+                code = response_json['error']
+                if code == 6:
+                    sleep(0.35)
+                    continue
+            else:
+                return response_json
+        except requests.exceptions.HTTPError:
+            print('HTTP Error')
+            continue
 
 
 def validate_user_name(user_name):
-    params = {
-        'access_token': TOKEN,
-        'v': VERSION,
-        'user_ids': user_name,
-    }
-    sleep(0.35)
-    print('-')
-    response = requests.get('https://api.vk.com/method/users.get', params)
-    response_json = response.json()
+    response_json = do_request('https://api.vk.com/method/users.get', user_ids=user_name)
     try:
         return response_json['response'][0]['id']
     except KeyError:
@@ -22,15 +45,7 @@ def validate_user_name(user_name):
 
 
 def get_user_friends_list(user_id=None):
-    params = {
-        'access_token': TOKEN,
-        'v': VERSION,
-        'user_id': user_id,
-    }
-    sleep(0.35)
-    print('-')
-    response = requests.get('https://api.vk.com/method/friends.get', params)
-    response_json = response.json()
+    response_json = do_request('https://api.vk.com/method/friends.get', user_id=user_id)
     try:
         return response_json['response']['items']
     except KeyError:
@@ -38,15 +53,7 @@ def get_user_friends_list(user_id=None):
 
 
 def get_user_groups_list(user_id=None):
-    params = {
-        'access_token': TOKEN,
-        'v': VERSION,
-        'user_id': user_id
-    }
-    sleep(0.35)
-    print('-')
-    response = requests.get('https://api.vk.com/method/groups.get', params)
-    response_json = response.json()
+    response_json = do_request('https://api.vk.com/method/groups.get', user_id=user_id)
     try:
         return response_json['response']['items']
     except KeyError:
@@ -54,15 +61,8 @@ def get_user_groups_list(user_id=None):
 
 
 def get_group_info(group_id):
-    params = {
-        'access_token': TOKEN,
-        'v': VERSION,
-        'group_id': group_id,
-        'fields': 'members_count'
-    }
-    sleep(0.35)
-    response = requests.get('https://api.vk.com/method/groups.getById', params)
-    response_json = response.json()
+    response_json = do_request('https://api.vk.com/method/groups.getById',
+                               group_id=group_id, fields='members_count')
     try:
         group_info = {
             'id': response_json['response'][0]['id'],
@@ -75,8 +75,7 @@ def get_group_info(group_id):
 
 
 def save_groups_to_json(groups):
-    import json
-    with open("groups.json", "w", encoding="utf-8") as file:
+    with open("groups.json", "w") as file:
         json.dump(groups, file)
     print('Файл сохранён')
 
@@ -86,16 +85,17 @@ def get_result_for_user(user_id):
     current_user_groups_list = set(get_user_groups_list(user_id))
     users_groups_list = []
     for friend_id in friends_list:
-        try:
-            users_groups_list.append(set(get_user_groups_list(friend_id)))
-        except TypeError:
+        if get_user_groups_list(friend_id) is None:
             continue
+        else:
+            users_groups_list.append(set(get_user_groups_list(friend_id)))
     users_groups_list_union = set.union(*users_groups_list)
     result_groups_list = current_user_groups_list.difference(users_groups_list_union)
     return result_groups_list
 
 
 if __name__ == '__main__':
+    TOKEN = get_token()
     print('Введите идентификатор или имя пользователя')
     user_name = input()
     validated_user_name = validate_user_name(user_name)
